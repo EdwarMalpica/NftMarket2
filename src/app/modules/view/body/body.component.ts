88e7fdaf331e7138e9c41cec2280354ea795c467
Manuel Alejandro Jiménez Acuña <manuel.jimenez01@uptc.edu.co>
2023-05-21 16:04:28 -0500

    Creacion de boton dinamico en conecion con waller

--- src/app/modules/view/body/body.component.ts
+++ src/app/modules/view/body/body.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ConexionService } from 'src/app/services/conexion.service';
+
 
 @Component({
   selector: 'app-body',
@@ -7,20 +9,34 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent {
-  isConected = false;
   showFiller = false;
-  constructor(private router: Router, private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.isConected = params['dato'];
-    });
+  addressUserView: boolean = false;
+  loginUser: boolean = false;
+  addressUser: string = '';
+  constructor(private cdr: ChangeDetectorRef, private router: Router, private route: ActivatedRoute,
+    private conexion:ConexionService) { }
+  
+  ngOnInit(): void {
+      this.conexion.loginUser.subscribe((res: boolean) => { 
+        this.loginUser = res;
+        (!this.loginUser) ? this.addressUserView = false : this.addressUserView = true;
+        this.cdr.detectChanges();
+      });
+      
+      this.conexion.addressUser.subscribe((res: string) => { 
+        this.addressUser = res;
+        this.cdr.detectChanges();
+      });
   }
 
-  actualizarVariablePadre(nuevoValor: boolean) {
-    this.isConected = nuevoValor;
+  getShortenedAddress(): string {
+    const maxLength = 10; // Longitud máxima de la dirección que deseas mostrar
+    if (this.addressUser.length > maxLength) {
+      // Utiliza el método 'substr' para recortar la dirección a la longitud deseada
+      return this.addressUser.substring(0, 5) + '...' + this.addressUser.substring(this.addressUser.length-5, this.addressUser.length);
+    }
+    return this.addressUser;
   }
-
   mostrarPresentation() {
     this.router.navigate(['/home']);
   }
@@ -32,7 +48,11 @@ export class BodyComponent {
     this.router.navigate(['/analytics']);
   }
   mostrarCuenta(){
-    this.router.navigate(['/account']);
+    this.router.navigate(['/account', this.addressUser]);
   }
 
+  cambiarValor() {
+    this.conexion.connect();
+    console.log("click");
+  }
 }
